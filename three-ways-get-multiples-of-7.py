c2bf869db4ffb57cb432842e4eb8a66fd2777787
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-10-14 21:50:31 +0200

    Put first way of three-ways.. into function

--- three-ways-get-multiples-of-7.py
+++ three-ways-get-multiples-of-7.py
@@ -1,15 +1,17 @@
 print('Tree ways to get multiples of 7') 
 print('1th - use while loop')
 
-multiples = []
-x = 0
-y = 0
-while x < 100:
-    y = x * 7
-    x += 1
-    if y < 100:
-        multiples.append(y)
+def calcMultiplesByWhileLoop():
+    multiples = []
+    x = 0
+    y = 0
+    while x < 100:
+        y = x * 7
+        x += 1
+        if y < 100:
+            multiples.append(y)
+    return multiples
 
-print (multiples)
+print(calcMultiplesByWhileLoop())
 
 print('end') 
