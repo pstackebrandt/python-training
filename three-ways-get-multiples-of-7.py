3597f692b2c63becf3f4655b07098d36cf192002
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-10-16 21:01:13 +0200

    3 ways to multiple: Add solution with list comprehension, small optimizations

--- three-ways-get-multiples-of-7.py
+++ three-ways-get-multiples-of-7.py
@@ -1,21 +1,25 @@
-print('Tree ways to get multiples of 7') 
-print('1th - use while loop')
+print('Three ways to get multiples of 7')
 
-def calcMultiplesByWhileLoop():
+
+
+def calcMultiplesByWhileLoop(multiplier, maxValue, minValue):
     multiples = []
-    x = 0
-    while x < 100:
-        y = x * 7
+    x = minValue
+    while x < maxValue:
+        y = x * multiplier
         x += 1
-        if y < 100:
+        if y < maxValue:
             multiples.append(y)
+        else:
+            break
             
     return multiples
 
-def calcMultiplesByForLoop(multiplier, maxValue):
+# max verbose solution
+def calcMultiplesByForLoop(multiplier, maxValue, minValue):
     multiples = []
     
-    for x in range(maxValue):
+    for x in range(minValue, maxValue):
         y = x * multiplier
         if y < maxValue:
             multiples.append(y)
@@ -30,12 +34,18 @@ def calcMultiplesByForLoopWithSteps(multiplier, maxValue, minValue):
 
     return multiples
 
+# best solution    
+def calcMultiplesByComprehension(multiplier, maxValue, minValue):
+    return [x * 7 for x in range(minValue, maxValue//multiplier + 1)]
 
 print('Result of:')
-print('Multiplication by while loop         : %s' % calcMultiplesByWhileLoop())
-resultOfFirstForLoop = calcMultiplesByForLoop(7, 100)
+print('Multiplication by while loop         : %s' % calcMultiplesByWhileLoop(7, 100, 0))
+resultOfFirstForLoop = calcMultiplesByForLoop(7, 100, 0)
 print(f'Multiplication by for loop           : {resultOfFirstForLoop}')
 print('Multiplication by for loop with steps: {}'.format(calcMultiplesByForLoopWithSteps(7, 100, 0)))
+print('Multiplication by list comprension:    {}'.format(calcMultiplesByComprehension(7, 100, 0)))
+
+print('\nDifferent values:')
 print('Multiplication by for loop with steps: {}'.format(calcMultiplesByForLoopWithSteps(7, 92, 14)))
 
 print('End of script') 
