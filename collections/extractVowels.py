64f75f7de6a481e8a3079d20f8efd5ae0b49fd8d
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-10-17 22:10:49 +0200

    Add 2 versions of extraction of vowels

--- collections/extractVowels.py
+++ collections/extractVowels.py
@@ -2,9 +2,30 @@
 
 WORDS = 'Hello, World!'
 ALL_VOWELS = ['a', 'e', 'i', 'o', 'u']
+ALL_VOWELS_STR = ''.join(ALL_VOWELS)
 
-vowels = [x for x in WORDS if x in ALL_VOWELS]
+def extractVowelsByListComprehension(words, allVowels):
+    return [x for x in words if x in allVowels]
 
-#print('Vowels found: %s in %s' % (vowels, WORDS)) #old style with %s and %
-print('Vowels found: {vowels} in {WORDS}')
- 
+vowelsFromCompre = extractVowelsByListComprehension(WORDS, ALL_VOWELS)
+print(f'Vowels found by list comprehension: {vowelsFromCompre} in {WORDS}')
+
+#print('Vowels found by list comprehension: %s in %s' % (vowelsFromCompre, WORDS)) #old style with %s and %
+
+
+def extractVowelsByFilterWithFunc(words, allVowelsStr):
+    def filterVowels(char):
+        if char in allVowelsStr:
+            return True
+    
+    return list(filter(filterVowels, words))
+
+vowelsFromFilter = extractVowelsByFilterWithFunc(WORDS, ALL_VOWELS_STR)
+print(f'Vowels found by filter with function: {vowelsFromFilter} in {WORDS}')
+
+
+def extractVowelsByFilterWithLambda(words, allVowelsStr):
+    return list(filter(lambda char: char in allVowelsStr, words))
+
+vowelsFromFilter = extractVowelsByFilterWithLambda(WORDS, ALL_VOWELS_STR)
+print(f'Vowels found by filter with lambda: {vowelsFromFilter} in {WORDS}')
