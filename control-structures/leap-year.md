006be8425fe06a04b7bccf5408a2ebf7b915607e
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-10-22 22:57:29 +0200

    leap-year.py: Add calculation of days of month

--- control-structures/leap-year.md
+++ control-structures/leap-year.md
@@ -24,14 +24,21 @@ month = 2 # 1 - January, ...
 print('der %d. Monat im Jahr %d hat %d Tage.' % (month, year, days))
 
 ### My rules
-- We check only years with 4 digits.
+
+We check only years with 4 digits.
+The user must enter a year and a month number.
+The month number must be between 1 and 12.
 
 ### Examples
 
-ChatGPT 4o w. Canvas Beta: Here are three years that match the conditions:
+From ChatGPT 4o w. Canvas Beta
 
-1600: Condition 1 - Divisible by 400 (True)
-1604: Condition 3 - Divisible by 4, but not by 100 (True)
-1608: Condition 3 - Divisible by 4, but not by 100 (True)
+Here are years that match the conditions:
+
+- 1600: Condition 1 - Divisible by 400 (True)
+- 1604: Condition 3 - Divisible by 4, but not by 100 (True)
+- 1608: Condition 3 - Divisible by 4, but not by 100 (True)
+  
+- 1700: Condition 2 - Divisible by 100, but not by 400 (False)
+
 The year 1600 meets the condition of being divisible by 400 and is therefore a leap year. The years 1604 and 1608 are divisible by 4, but not by 100, and are also leap years. A year that is only divisible by 100 (but not by 400) would be, for example, 1700, which is not a leap year.
-
--- control-structures/leap-year.py
+++ control-structures/leap-year.py
@@ -3,6 +3,32 @@
 
 # Create a simple leap year test(Schaltjahr).
 # Task by Kofler:Python.-2024, 9.7. W1 p. 135, 151
+# https://de.wikipedia/wiki/Schaltjahr
+
+# Tasks:
+# todo Add tests
+# todo    for each leap and not leap years
+# todo    for days of month (leap, not leap, different month)
+# ok: Get month (1-12) from user
+#   ok: Check input
+#   Monatsname bei Eingabeauswertung anzeigen
+# ok: calculate days
+# ok: print days
+
+MONTHS_DATA = (
+    ('Januar', 'January', 1, 31),
+    ('Februar', 'February', 2, 28),
+    ('März', 'March', 3, 31),
+    ('April', 'April', 4, 30),
+    ('Mai', 'May', 5, 31),
+    ('Juni', 'June', 6, 30),
+    ('Juli', 'July', 7, 31),
+    ('August', 'August', 8, 31),
+    ('September', 'September', 9, 30),
+    ('Oktober', 'October', 10, 31),
+    ('November', 'November', 11, 30),
+    ('Dezember', 'December', 12, 31)
+)
 
 def getYearFromUser():
     print("""Bitte gebe das zu prüfende Jahr ein!
@@ -36,6 +62,38 @@ def getYearFromUser():
         
     return year
 
+def getMonthNumberFromUser():
+    print("""Für welchen Monat sollen die Tage berechnet werden?
+    Nutze bitte die Zahlen 1 - 12""")
+    
+    # ask for month (if not done 3 times already)
+    askTimesLeft = 3
+    monthNumber = None
+    while(askTimesLeft > 0 and monthNumber == None):
+        monthIn = input("Monat: ")
+        askTimesLeft -= 1 
+        # check input
+        try: 
+            monthNumber = int(monthIn)
+            # check size of monthNumber
+            if monthNumber < 1:
+                print(f"Die Monatsnummer {monthNumber} ist zu klein.")
+                monthNumber = None
+            elif monthNumber > 12:
+                print(f"Die Monatsnummer {monthNumber} ist zu groß.")
+                monthNumber = None
+            else:
+                monthName = getMonthName(monthNumber)
+                print(f"Die Monatsnummer {monthNumber} vom {monthName} kann verwendet werden.")
+        except Exception as e: 
+            print(f"""Die Eingabe '{monthIn}' ist leider keine Zahl.
+    Versuche es noch einmal!""")
+        finally:
+            if monthNumber == None:
+                tries = 'Versuche' if askTimesLeft == 2 else 'Versuch' 
+                print(f"Du hast noch {askTimesLeft} {tries}.")
+    return monthNumber
+
 def isLeapYear(year):
     if year % 400 == 0:
         return True
@@ -44,18 +102,44 @@ def isLeapYear(year):
     elif year % 4 == 0: 
         return True
 
-def outputResults(year, isLeapYear):
+def getDaysOfMonth(monthNumber, isLeapYear):
+    if isLeapYear and monthNumber == 2:
+       return 27
+    else:    
+       return MONTHS_DATA[monthNumber-1][3]
+
+def getMonthName(monthNumber):
+    return MONTHS_DATA[monthNumber-1][0]
+
+def outputLeapYear(year, isLeapYear):
     # print results
     if isLeap: 
         print(f"Das Jahr {year} ist ein Schaltjahr.")
     else:
         print(f"{year} ist kein Schaltjahr.")
 
+def outputDaysOfMonth(days, monthNumber, year, isLeap):
+    print(f"Der {getMonthName(monthNumber)} {year} hat {days} Tage.")
+
+'''
+Show days of month! Use this template!
+year = 2024
+month = 2 # 1 - January, ... 
+...
+print('der %d. Monat im Jahr %d hat %d Tage.' % (month, year, days))
+'''
+
 print("Schaltjahrtest")
+
 year = getYearFromUser()
-print(f"Das Jahr {year} wird geprüft.")
+monthNumber = getMonthNumberFromUser()
+print(f"Das Jahr {year} und der Monat {monthNumber} werden geprüft.")
+
 isLeap = isLeapYear(year)
-outputResults(year, isLeap)
+days = getDaysOfMonth(monthNumber, isLeap)
+print()
+outputLeapYear(year, isLeap)
+outputDaysOfMonth(days, monthNumber, year, isLeap)
 
 
 
